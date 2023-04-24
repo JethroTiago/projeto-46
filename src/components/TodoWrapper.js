75f9dcbea35eb1b84d03a84a7de21ede5b99f28d
Jethro <jethrotiago@hotmail.com>
2023-04-24 12:40:07 -0300

    Changed the initial code and imported TodoForm component

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { TodoForm } from './TodoForm'
 
-export const TodoWrapper = () => {
+const TodoWrapper = () => {
   return (
     <div>
       <TodoForm />
     </div>
   )
 }
+
+export default TodoWrapper
