bb1b9c6ea07f805ce7e1c301236e85f83c1caf6e
Jethro <jethrotiago@hotmail.com>
2023-04-24 13:40:29 -0300

    Imported useState and created a variable to use it

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TodoForm from './TodoForm'
 
 const TodoWrapper = () => {
+  const [todos, setTodos] = useState([])
   return (
     <div className='TodoWrapper'>
       <TodoForm />
