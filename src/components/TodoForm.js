d8bcc175bb49b08c658d28083f5dcdfea40d2e13
Jethro <jethrotiago@hotmail.com>
2023-04-24 13:37:56 -0300

    Created variable handlesubmit to control the form submited

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,8 +2,13 @@ import React, {useState} from 'react'
 
 const TodoForm = () => {
   const [value, setValue] = useState("")
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(value)
+  }
+
   return (
-    <form className='TodoForm'>
+    <form className='TodoForm' onSubmit={handleSubmit}>
       <input type='text' className='todo-input' placeholder='O que vai fazer hoje?' onChange={(e) => setValue(e.target.value)} />
       <button type='submit' className='todo-btn'>Adicionar</button>
     </form>
