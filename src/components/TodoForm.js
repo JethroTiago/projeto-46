745c4ae80be69040f940730aee413963f46d3297
Jethro <jethrotiago@hotmail.com>
2023-04-24 12:44:38 -0300

    Added a button name and imported useState

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const TodoForm = () => {
   return (
     <form className='TodoForm'>
       <input type='text' className='todo-input' placeholder='O que vai fazer hoje?' />
-      <button type='submit' className='todo-btn' />
+      <button type='submit' className='todo-btn'>Adicionar</button>
     </form>
   )
 }
