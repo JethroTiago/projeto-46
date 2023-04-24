fe70a2ef2a04074843ab37901a4fa944b9fd3938
Jethro <jethrotiago@hotmail.com>
2023-04-24 13:31:42 -0300

    Implemented function the the input to capture the event value

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,7 +4,7 @@ const TodoForm = () => {
   const [value, setValue] = useState("")
   return (
     <form className='TodoForm'>
-      <input type='text' className='todo-input' placeholder='O que vai fazer hoje?' onChange={(e) => console.log(e.target.value)} />
+      <input type='text' className='todo-input' placeholder='O que vai fazer hoje?' onChange={(e) => setValue(e.target.value)} />
       <button type='submit' className='todo-btn'>Adicionar</button>
     </form>
   )
