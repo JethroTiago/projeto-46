4bb1a33ea4bea69366eaacb191b50ccd1a59d84a
Jethro <jethrotiago@hotmail.com>
2023-04-26 21:41:38 -0300

    Created a function that start the text editing when the pen icon is clicked

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import TodoForm from './TodoForm'
 import { v4 as uuidv4 } from 'uuid'
 import Todo from './Todo';
+import { editableInputTypes } from '@testing-library/user-event/dist/utils';
 uuidv4();
 
 const TodoWrapper = () => {
@@ -15,6 +16,10 @@ const TodoWrapper = () => {
     setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo))
   }
 
+  const editTodo = id => {
+    setTodos(todos.map(todo => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+  }
+
   const deleteTodo = id => {
     setTodos(todos.filter(todo => todo.id !== id))
   }
@@ -24,7 +29,7 @@ const TodoWrapper = () => {
       <h1>Get Things Done!</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
+        <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo}/>
       ))}
     </div>
   )
